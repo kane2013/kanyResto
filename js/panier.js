c3edcf693cca91dd326e8cca276199049ab8da13
kane2013 <bousam79@hotmail.fr>
2023-04-14 12:02:58 +0200

    update and commentaire

--- js/panier.js
+++ js/panier.js
@@ -1,30 +1,36 @@
 window.onload = function(){
 
-    if (localStorage.getItem("listeProduit")){
-        let listeProduit = JSON.parse(localStorage.getItem("listeProduit"));
-        let bodyPanier = document.getElementById("bodyPanier");
-        let total = 0;
-        listeProduit.map( product => {
-            total+=product.price
+    if (localStorage.getItem("listeProduit")){ // Si il existe une liste de produit dans mon localStorage
+        let listeProduit = JSON.parse(localStorage.getItem("listeProduit")); // Convertie la liste du format STRING en JSON
+        let bodyPanier = document.getElementById("bodyPanier"); // Body tu tableau de panier
+        let total = 0; // Prix total
+        listeProduit.map( product => { // Pour chasue produit de ma liste de produit
+            total+=product.price // J'ajoute le prix du produit au prix total
             bodyPanier.innerHTML+=`<tr>
                                         <th scope="row"><img src="${product.image}" width="300px"/></th>
                                         <td>${product.name}</td>
                                         <td>${product.price}€</td>
-                                    </tr>`
+                                    </tr>` // Ajoute a mon Tableau une ligne correspondant à mon produit
         })
+
         bodyPanier.innerHTML+=`<tr>
-                                        <th scope="row" colspan="2">TOTAL</th>
-                                        <td>${total}€</td>
-                                    </tr>`
+                                    <th scope="row" colspan="2">TOTAL</th>
+                                    <td>${total}€</td>
+                                </tr>` // Ajoute la ligne du total
+
+        // Méthode sans template String                                
+        // bodyPanier.innerHTML+="<tr>"+
+        //                         "<th scope='row' colspan='2'>TOTAL</th>"+
+        //                         "<td>$"+total+"€</td>"+
+        //                     "</tr>" /
     }else{
-        body = document.getElementById("body");
-        body.innerHTML="<h2>Votre panier est vide, retournez à la page d'acceuil</h2>"
+        document.getElementById("body").innerHTML="<h2>Votre panier est vide, retournez à la page d'acceuil</h2>"
     }
     
 }
 
 
-function validerPannier(){
-    localStorage.removeItem("listeProduit");
-    window.location.href="./fin.html"
+function validerPanier(){
+    localStorage.removeItem("listeProduit"); // Supprime le panier du localStorage
+    window.location.href="./fin.html" // Redirection vers la page de fin
 }
--- js/script.js
+++ js/script.js
@@ -1,75 +1,94 @@
-window.onload = function(){
+window.onload = function(){ // Quand la page à fini de charger
 
-    const slide = ["./images/devanture.jpg", "https://loremflickr.com/1802/1002", "https://loremflickr.com/1804/1000", "https://loremflickr.com/1805/1000"];
-let numero = 0;
+    let categories = document.getElementById("categories")
 
-let categories = document.getElementById("categories")
-
-
-fetch("data.json").then(
-    response => response.json())
-    .then( 
-        data => {
-            data.categories.map( categorie => {
-                
-                let htmlProduct = "";
-                categorie.products.map( product => {
-                    htmlProduct+=`
-                    <div class="card col-4" style="width: 18rem;">
-                        <img src="${product.image}" class="card-img-top" alt="...">
-                        <div class=" card-body text-center">
-                            <p class="card-text">${product.name}</p>
-                            <p class="price">${product.price}€</p>
-                            <button onclick='addToPanier(${JSON.stringify(product)})'>Ajouter</button>
+    // Fait un appel asyncrone pour récuperer les données du fichier data.json
+    fetch("data.json").then(
+        response => response.json()) // Converti la reponse au format JSON
+        .then( 
+            data => {
+                data.categories.map( categorie => { // Pour chaque catégorie
+                    
+                    let htmlProducts = "";
+                    categorie.products.map( product => { // Pour chaque produit de cette catégorie
+                        htmlProducts+=`
+                        <div class="rowElement">
+                        <div class="card " style="width: 18rem;">
+                            <img src="${product.image}" class="card-img-top" alt="${product.name}">
+                            <div class=" card-body text-center">
+                                <p class="card-text">${product.name}</p>
+                                <p class="price">${product.price}€</p>
+                                <button onclick='addToPanier(${JSON.stringify(product)})'>Ajouter</button>
+                            </div>
                         </div>
-                    </div>
-                    `
-                })
-                let htmlCategies=`
-                <h2 id="first-title">${categorie.nom}</h2>
-                <div class="row mt-4 justify-content-around">
-                    ${htmlProduct}
-                </div>
-                `
-                categories.innerHTML+=htmlCategies
+                        </div>
+                        ` // J'ajoute la carte à liste html de produit
+                    })
+                    let htmlCategies=`
+                        <h2 id="first-title">${categorie.nom}</h2>
+                        <div class=" mt-4 d-flex justify-content-around">
+                            ${htmlProducts}
+                        </div>
+                        ` // Crée le html de la catégorie en y ajoutant les produits associés
+                        categories.innerHTML+=htmlCategies // Ajoute la catégorie à ma page
 
-            } )
-        }
+                    } )
+                }
 
+                    
             
-    
-)
+        )
+
+    setInterval(() => { // Appel la function ChangeSlide toute les 2 secondes
+        ChangeSlide(1)
+    }, 2000);
 
 
 
+}
+
+
+// Liste des images
+const slide = ["./images/devanture.jpg", "https://loremflickr.com/1802/1002", "https://loremflickr.com/1804/1000", "https://loremflickr.com/1805/1000"]; 
+let numero = 0;
 
 function ChangeSlide(sens) {
     numero = numero + sens;
-    if (numero < 0)
-    numero = slide.length - 1;
-    if (numero > slide.length - 1)
-    numero = 0;
+    if (numero < 0){
+        numero = slide.length - 1;
+    }
+    if (numero > slide.length - 1){
+        numero = 0;
+    }
     document.getElementById("slide").src = slide[numero];
 }
 
 
 
-}
+
+
+
+
 
 function addToPanier(product){
+    /*
+    Permet d'ajouter un produit au panier
+    */
     let listeProduit = []
-    if ( localStorage.getItem("listeProduit")){
-        listeProduit = JSON.parse(localStorage.getItem("listeProduit"))
+    if ( localStorage.getItem("listeProduit")){ // Si il existe une liste de produit dans mon localStorage
+        listeProduit = JSON.parse(localStorage.getItem("listeProduit")) // Je la recupere
     }
 
-    listeProduit.push(product);
+    listeProduit.push(product); // Ajoute mon produit à ma liste
+    localStorage.setItem("listeProduit", JSON.stringify(listeProduit) ) // met la liste dans le localStorage du navigateur
+
     const toastLiveExample = document.getElementById('liveToast')
     const messageToast = document.getElementById('messageToast')
-    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-    messageToast.innerText=`${product.name} a eté ajouter à votre panier`
-    toastBootstrap.show()
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample) // Créer un Toast ou recupere le celui egistant
+    messageToast.innerText=`${product.name} a eté ajouter à votre panier` // modifie le text du toast
+    toastBootstrap.show() // affiche le toast
 
 
-    localStorage.setItem("listeProduit", JSON.stringify(listeProduit) )
+    
        
 }
